46616b3aa1f43cba36dfdb642f34a15e92a6898b
Kevin <736074781@qq.com>
2017-11-30 14:55:34 +0000

    Finish user sign-up

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -21,4 +21,22 @@ class UserController extends Controller
       */
       return view('users.show', compact('user'));
     }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:15',
+            'email' => 'required|email|unique:users|max:30',
+            'password' => 'required|confirmed|max:15|min:6'
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password)
+        ]);
+
+        session()->flash('success', '欢迎， 你将在这里开始一段新的旅程~');
+        return redirect()->route('users.show', [$user]);
+    }
 }
